fbc441154c4e73adfc02f0cbb0c4fe19703782fa
Dany <ekdus2492@naver.com>
2023-08-24 21:55:15 +0900

    #44 아이템 추가됨 뜨는데 실제로 추가되지 않는 오류

--- app/src/main/java/kr/co/hs/businfo/feature/main/BusStationInfoActivity.kt
+++ app/src/main/java/kr/co/hs/businfo/feature/main/BusStationInfoActivity.kt
@@ -18,6 +18,7 @@ import kr.co.hs.businfo.viewmodel.BusStationViewModel
 class BusStationInfoActivity : AppCompatActivity() {
     private lateinit var busListAdapter: BusListAdapter
     private lateinit var busStationViewModel: BusStationViewModel
+    private lateinit var favoriteFragment: FavoriteFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +27,15 @@ class BusStationInfoActivity : AppCompatActivity() {
         val busStationId = intent.getIntExtra("busStationId", -1)
         busStationViewModel = ViewModelProvider(this).get(BusStationViewModel::class.java)
 
+        favoriteFragment = FavoriteFragment() // 여기서 초기화
+
         if (busStationId != -1) {
-            // 정류소 ID를 사용하여 정류소 정보 가져오기
             lifecycleScope.launch {
                 val busStationList = busStationViewModel.getBusStationById(busStationId)
                 if (busStationList.isNotEmpty()) {
                     val busStation = busStationList[0]
-                    showBusList(busStation.transitBus) // transitBus 정보 사용
+                    showBusList(busStation.transitBus)
 
-                    // 정류소 이름 설정
                     val busNameTextView = findViewById<TextView>(R.id.busName)
                     busNameTextView.text = busStation.stationName
 
@@ -43,37 +44,30 @@ class BusStationInfoActivity : AppCompatActivity() {
                     favoriteButton.setOnClickListener {
                         val busStopName = busNameTextView.text.toString()
 
+                        favoriteFragment.addFavoriteItem(busStopName)
 
-                        val favoriteFragment = supportFragmentManager.findFragmentById(R.id.favorite) as? FavoriteFragment
-                        favoriteFragment?.addFavoriteItem(busStopName)
                         Toast.makeText(this@BusStationInfoActivity, "즐겨찾기 추가되었습니다.", Toast.LENGTH_SHORT).show()
-                        Log.d("BusStationInfoActivity", "버스 정류장 이름: $busStopName") // 추가된 로그
+                        Log.d("BusStationInfoActivity", "버스 정류장 이름: $busStopName")
                     }
-
                 } else {
                     Log.d("BusStationInfoActivity", "No bus station found with ID: $busStationId")
-                    // 해당 ID에 해당하는 정류소를 찾을 수 없는 경우 처리
                 }
             }
         } else {
             Log.d("BusStationInfoActivity", "busStationId is invalid")
-            // busStationId가 유효하지 않은 경우 처리
         }
     }
 
     private fun showBusList(busList: List<String>) {
-        // RecyclerView를 찾아서 어댑터를 설정하여 표시
         val recyclerView = findViewById<RecyclerView>(R.id.rvBusNumbers)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         busListAdapter = BusListAdapter(this, busList)
         recyclerView.adapter = busListAdapter
 
-        // 리스트 아이템 클릭 이벤트 처리
         busListAdapter.setOnItemClickListener(object : BusListAdapter.OnItemClickListener {
             override fun onItemClick(busNumber: String) {
                 if (busNumber == "102") {
-                    // 102번을 눌렀을 때 NosunActivity로 이동
                     val intent = Intent(this@BusStationInfoActivity, NosunActivity::class.java)
                     startActivity(intent)
                 }
--- app/src/main/java/kr/co/hs/businfo/feature/main/FavoriteFragment.kt
+++ app/src/main/java/kr/co/hs/businfo/feature/main/FavoriteFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.hs.businfo.feature.main
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,13 +13,13 @@ import kr.co.hs.businfo.R
 
 class FavoriteFragment : Fragment() {
     private lateinit var favoriteListAdapter: FavoriteListAdapter
-    private val favoriteItems: MutableList<String> = mutableListOf("한밭대학교")
+    private val favoriteItems: MutableList<String> = mutableListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.favorite, container, false)
-
-        // RecyclerView 설정
         val recyclerView = view.findViewById<RecyclerView>(R.id.favorite_list)
+
+        // 어댑터 초기화
         favoriteListAdapter = FavoriteListAdapter(requireContext(), favoriteItems)
         recyclerView.adapter = favoriteListAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -28,7 +29,9 @@ class FavoriteFragment : Fragment() {
 
     fun addFavoriteItem(item: String) {
         favoriteItems.add(item)
-        Log.d("FavoriteFragment", "아이템 추가됨: $item") // 호출 시점 로그 출력
-        favoriteListAdapter.notifyDataSetChanged() // 변경된 내용을 어댑터에 알립니다.
+        Log.d("FavoriteFragment", "아이템 추가됨: $item")
+
+        // 어댑터 업데이트
+        favoriteListAdapter.notifyDataSetChanged()
     }
 }
